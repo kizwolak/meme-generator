6055636aeefcd9369e4a60ff28bf64cee46eeec7
kizwolak <ki.zwolak@gmail.com>
2023-02-18 20:02:36 -0300

    Log random meme to console

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,18 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+    const memes = memesData.data.memes
+    function getRandomImage(memes) {
+        console.log(memes[Math.floor(Math.random()*memes.length)]);
+    }
     return (
         <div className="form">
-            <form>
                 <div className="inputs">
                     <input type='text'></input>
                     <input type='text'></input>
                 </div>
-                <button className="new-image">Get a new meme image <img src="./images/icons8-picture.svg" alt="icon"/></button>
-            </form>
+                <button className="new-image" onClick={() => getRandomImage(memes)}>Get a new meme image <img src="./images/icons8-picture.svg" alt="icon"/></button>
         </div>
     )
 }
