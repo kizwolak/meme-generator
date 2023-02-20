5c86b02df137c32aea2e677d5306834db6661375
kizwolak <ki.zwolak@gmail.com>
2023-02-20 16:10:28 -0300

    Put all const into state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,11 +2,22 @@ import React, { useState } from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
-    const [img, setImg] = useState();
-    const memes = memesData.data.memes
+    // const [img, setImg] = useState();
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '',
+        randomImage: 'http://i.imgflip.com/1bij.jpg'
+    });
+    const [allMemesData, setAllMemesData] = useState(memesData.data.memes);
+    
     function getRandomImage(memes) {
-        const random = memes[Math.floor(Math.random()*memes.length)]
-        setImg(random.url);
+        const random = allMemesData[Math.floor(Math.random()*memes.length)]
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                randomImage: random.url
+            }
+        })
     }
     return (
         <div className="form">
@@ -14,8 +25,8 @@ export default function Meme() {
                 <input type='text'></input>
                 <input type='text'></input>
             </div>
-            <button className="new-image" onClick={() => getRandomImage(memes)}>Get a new meme image <img src="./images/icons8-picture.svg" alt="icon"/></button>
-            <img src={img} className='meme-img' alt="meme placeholder"/>
+            <button className="new-image" onClick={() => getRandomImage(allMemesData)}>Get a new meme image <img src="./images/icons8-picture.svg" alt="icon"/></button>
+            <img src={meme.randomImage} className='meme-img' alt="meme placeholder"/>
         </div>
     )
 }
